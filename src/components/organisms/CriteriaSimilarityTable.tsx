cc6787e385cdc07bb37a8e10932ca9f4c32bddac
Martin Kapal <kapal@cngroup.dk>
2020-11-12 01:24:38 +0100

    Use Material UI table

--- src/components/organisms/CriteriaSimilarityTable.tsx
+++ src/components/organisms/CriteriaSimilarityTable.tsx
@@ -1,3 +1,12 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
 import isArray from 'lodash/isArray';
 import React from 'react';
 
@@ -27,72 +36,80 @@ export function CriteriaSimilarityTable({
   const frameworks = Object.keys(frameworkData);
 
   return (
-    <table style={{ width: '100%' }}>
-      <tbody>
-        <tr>
-          <th>Criterion</th>
-          <th>Weight</th>
-          <th>Submitted value</th>
-          {rankings.map(({ framework }) => (
-            <th key={framework}>{displayStrings[framework]}</th>
-          ))}
-        </tr>
-        {Object.keys(formData).map((category) => {
-          const criteria = Object.keys(formData[category]);
+    <TableContainer component={Paper}>
+      <Table style={{ width: '100%' }} size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Criterion</TableCell>
+            <TableCell>Weight</TableCell>
+            <TableCell>Submitted value</TableCell>
+            {rankings.map(({ framework }) => (
+              <TableCell key={framework}>{displayStrings[framework]}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(formData).map((category) => {
+            const criteria = Object.keys(formData[category]);
 
-          return (
-            <React.Fragment key={category}>
-              <tr>
-                <td colSpan={5}>
-                  <strong>{displayStrings[category]}</strong>
-                </td>
-              </tr>
-              {criteria.map((criterion) => {
-                const value = formData[category][criterion];
-                const weight = weights[category][criterion] ?? 0;
+            return (
+              <React.Fragment key={category}>
+                <TableRow>
+                  <TableCell colSpan={5}>
+                    <strong>{displayStrings[category]}</strong>
+                  </TableCell>
+                </TableRow>
+                {criteria.map((criterion) => {
+                  const value = formData[category][criterion];
+                  const weight = weights[category][criterion] ?? 0;
 
-                return (
-                  <tr key={criterion}>
-                    <td>{displayStrings[criterion]}</td>
-                    <td>{weight}</td>
-                    <td>
-                      {isArray(value)
-                        ? value.join(', ')
-                        : (value as string).toString()}
-                    </td>
-                    {frameworks.map((framework) => {
-                      const frameworkValue =
-                        frameworkData[framework].criteria[category][criterion];
+                  return (
+                    <TableRow key={criterion}>
+                      <TableCell>{displayStrings[criterion]}</TableCell>
+                      <TableCell>{weight}</TableCell>
+                      <TableCell>
+                        {isArray(value)
+                          ? value.join(', ')
+                          : (value as string).toString()}
+                      </TableCell>
+                      {frameworks.map((framework) => {
+                        const frameworkValue =
+                          frameworkData[framework].criteria[category][
+                            criterion
+                          ];
 
-                      const criterionScore =
-                        rankings.find(
-                          (ranking) => ranking.framework === framework,
-                        )?.criteria[category][criterion] ?? 0;
+                        const criterionScore =
+                          rankings.find(
+                            (ranking) => ranking.framework === framework,
+                          )?.criteria[category][criterion] ?? 0;
 
-                      return (
-                        <td key={framework}>
-                          ({criterionScore.toFixed(2)}){' '}
-                          {isArray(frameworkValue)
-                            ? frameworkValue.join(', ')
-                            : frameworkValue}
-                        </td>
-                      );
-                    })}
-                  </tr>
-                );
-              })}
-            </React.Fragment>
-          );
-        })}
-        <tr>
-          <td>-</td>
-          <td>-</td>
-          <td>-</td>
-          {rankings.map(({ framework, totalSimilarity }) => (
-            <td key={framework}>{totalSimilarity.toFixed(2)}</td>
-          ))}
-        </tr>
-      </tbody>
-    </table>
+                        return (
+                          <TableCell key={framework}>
+                            ({criterionScore.toFixed(2)}){' '}
+                            {isArray(frameworkValue)
+                              ? frameworkValue.join(', ')
+                              : frameworkValue}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+              </React.Fragment>
+            );
+          })}
+          <TableRow>
+            <TableCell>-</TableCell>
+            <TableCell>-</TableCell>
+            <TableCell>-</TableCell>
+            {rankings.map(({ framework, totalSimilarity }) => (
+              <TableCell key={framework}>
+                {totalSimilarity.toFixed(2)}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
